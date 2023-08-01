9caa7c31cfc654756c500ba5c8c86d8a73f10b82
NaraeJang <j.naaaarae@gmail.com>
2023-08-01 17:04:03 -0400

    Final update

--- src/displaySingleDrink.js
+++ src/displaySingleDrink.js
@@ -4,16 +4,31 @@ import get from "./getElement.js";
 const singleDrinkSection = get(".single-drink");
 
 const displaySingleDrink = (data) => {
+  hideloading();
+
   const theDrink = data.drinks[0];
   const { strDrinkThumb: img, strDrink: name, strInstructions: des } = theDrink;
+  const list = [
+    theDrink.strIngredient1,
+    theDrink.strIngredient2,
+    theDrink.strIngredient3,
+    theDrink.strIngredient4,
+    theDrink.strIngredient5,
+  ];
 
-  hideloading();
+  const drinkList = list
+    .map((item) => {
+      if (!item) return;
+
+      return `<li><i class="far fa-check-square"></i>${item}</li>`;
+    })
+    .join("");
 
   singleDrinkSection.innerHTML = `<img src="${img}" alt="${name}" class="drink-img">
   <article class="drink-info">
     <h2 class="drink-name">${name}</h2>
     <p class="drink-desc">${des}</p>
-    <ul class="drink-ingredients"><a href="index.html" class="btn">all cocktails</a></ul>`;
+    <ul class="drink-ingredients">${drinkList}<a href="index.html" class="btn">all cocktails</a></ul>`;
 };
 
 export default displaySingleDrink;
