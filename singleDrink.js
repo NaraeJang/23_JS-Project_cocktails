f19e0fe47aa7eb6ca644003bac438c8deaef90ff
NaraeJang <j.naaaarae@gmail.com>
2023-08-01 16:46:41 -0400

    update single Drink.js

--- singleDrink.js
+++ singleDrink.js
@@ -1,23 +1,13 @@
 import fetchDrinks from "./src/fetchDrinks.js";
-import get from "./src/getElement.js";
-import { hideloading } from "./src/toggleLoading.js";
+
+import displaySingleDrink from "./src/displaySingleDrink.js";
 
 const id = localStorage.getItem("drink");
 const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
-const singleDrinkSection = get(".single-drink");
 
 window.addEventListener("DOMContentLoaded", async () => {
   const data = await fetchDrinks(url);
-  const theDrink = data.drinks[0];
 
   //   display drink
-  const { strDrinkThumb: img, strDrink: name, strInstructions: des } = theDrink;
-
-  hideloading();
-
-  singleDrinkSection.innerHTML = `<img src="${img}" alt="${name}" class="drink-img">
-  <article class="drink-info">
-    <h2 class="drink-name">${name}</h2>
-    <p class="drink-desc">${des}</p>
-    <ul class="drink-ingredients"><a href="index.html" class="btn">all cocktails</a></ul>`;
+  displaySingleDrink(data);
 });
--- src/displaySingleDrink.js
+++ src/displaySingleDrink.js
@@ -1,5 +1,19 @@
-import fetchDrinks from "./fetchDrinks.js";
+import { hideloading } from "./toggleLoading.js";
+import get from "./getElement.js";
 
-window.addEventListener("DOMContentLoaded", () => {
-  fetchDrinks();
-});
+const singleDrinkSection = get(".single-drink");
+
+const displaySingleDrink = (data) => {
+  const theDrink = data.drinks[0];
+  const { strDrinkThumb: img, strDrink: name, strInstructions: des } = theDrink;
+
+  hideloading();
+
+  singleDrinkSection.innerHTML = `<img src="${img}" alt="${name}" class="drink-img">
+  <article class="drink-info">
+    <h2 class="drink-name">${name}</h2>
+    <p class="drink-desc">${des}</p>
+    <ul class="drink-ingredients"><a href="index.html" class="btn">all cocktails</a></ul>`;
+};
+
+export default displaySingleDrink;
